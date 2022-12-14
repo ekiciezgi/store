769686af9da7db513aa71a286646c2b14a6afe46
ezgi <ekiciezgi3@icloud.com>
2022-12-15 01:48:19 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Pages/Home/Home";
+import Product from "./Pages/Product/Product";
+
+import Products from "./Pages/Products/Products";
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/products/:id",
+        element: <Products/>,
+      },
+      {
+        path: "/product/:id",
+        element: <Product/>,
+      }
+    ]
+  }
+]);
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
